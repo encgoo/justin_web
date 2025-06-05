7e5c33e4d5cd350ada9ec2e1ba651f127874bf94
Yongjian Feng <yongjianfeng@yongjians-mbp.mynetworksettings.com>
2025-06-04 21:38:41 -0400

    New

--- js/script.js
+++ js/script.js
@@ -1,39 +1,64 @@
 // query all the elements
-const leftBtn = document.querySelector('.slide-left');
-const rightBtn = document.querySelector('.slide-right');
-const slides = document.querySelectorAll('.slide');
+const leftBtn = document.querySelector(".slide-left");
+const rightBtn = document.querySelector(".slide-right");
+const slides = document.querySelectorAll(".slide");
+const dotbtns = document.querySelectorAll(".dot-btn");
 
+let cur = 0;
+leftBtn.disabled = true;
 
-const positions = ["-200%", "-100%", "0%", "100%", "200%"];
-let curFirstPos = 2;
-
-
-leftBtn.addEventListener('click', function(e) {
-    if (curFirstPos > 0) {
-        curFirstPos --;
-    }
-    slides.forEach((slide,idx) => {
-        if (slide.id == "slide-1") {
-            slide.style.transform = 'translateX(-200%)';
-        } else if (slide.id == "slide-2") {
-            slide.style.transform = 'translateX(-100%)';
-        } else {
-            slide.style.transform = 'translateX(0%)';
-        }
+function deactiveDotBtns() {
+    dotbtns.forEach((btn) => {
+        btn.classList.remove("active");
     });
+}
+
+function updateSlice() {
+    slides.forEach((slide, idx) => {
+        const delta = idx - cur;
+        const percentage = String(delta) + "00%";
+        slide.style.transform = `translateX(${percentage})`;
+    });
+}
+
+
+function dotBtnOnClick(e) {
+    leftBtn.disabled = false;
+    rightBtn.disabled = false;
+    cur = Number(e.target.id.slice(-1))-1;
+
+    if (cur == 0) {
+        leftBtn.disabled = true;
+    } else if (cur == 6) {
+        rightBtn.disabled = true;
+    }
+    deactiveDotBtns();
+    e.target.classList.add("active");
+    updateSlice();
+}
+dotbtns.forEach((btn) => {
+    btn.addEventListener("click", dotBtnOnClick);
 });
 
-rightBtn.addEventListener('click', function(_) {
-    if (curFirstPos < 2) {
-        curFirstPos ++;
+leftBtn.addEventListener("click", function (e) {
+    cur--;
+    rightBtn.disabled = false;
+    deactiveDotBtns();
+    dotbtns[cur].classList.add("active");
+
+    if (cur == 0) {
+        leftBtn.disabled = true;
     }
-    slides.forEach(slide => {
-        if (slide.id == "slide-1") {
-            slide.style.transform = 'translateX(-100%)';
-        } else if (slide.id == "slide-2") {
-            slide.style.transform = 'translateX(0%)';
-        } else {
-            slide.style.transform = 'translateX(100%)';
-        }
-    });
-})
+    updateSlice();
+});
+
+rightBtn.addEventListener("click", function (_) {
+    cur++;
+    leftBtn.disabled = false;
+    if (cur == 6) {
+        rightBtn.disabled = true;
+    }
+    deactiveDotBtns();
+    dotbtns[cur].classList.add("active");
+    updateSlice();
+});
